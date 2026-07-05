49b42ce79656262e5ef0314081a6f11df437ef77
Shatayu Khante <shatayukhante@gmail.com>
2026-07-05 08:42:08 +0530

    fixed typo

--- frontend/src/components/layout/UserLayout.jsx
+++ frontend/src/components/layout/UserLayout.jsx
@@ -1,20 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../common/Footer";
-import Header from "../common/header";
+import Header from "../common/Header";
 
 const UserLayout = () => {
-    return (
-        <>
-            {/* Header */}
-            <Header />
-            {/* Main content */}
-            <main>
-                <Outlet/>
-            </main>
-            {/* Footer */}
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      {/* Main content */}
+      <main>
+        <Outlet />
+      </main>
+      {/* Footer */}
+      <Footer />
+    </>
+  );
 };
 
 export default UserLayout;
